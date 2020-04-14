89be52f0b58e39878e5a8ebb51d1242ef065052b
Kevin Stradtman <stradtkt22@gmail.com>
2020-04-13 20:43:00 -0400

    Added the key functionality for coindex

--- commands/key.js
+++ commands/key.js
@@ -1,12 +1,44 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyManager = require('../lib/KeyManager');
+const {isRequired} = require('../utils/validation');
+
+
 const key = {
-    set() {
-        console.log('Hello from set');
+    async set() {
+        const keyManager = new KeyManager();
+        const input = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Enter API Key'.green + ' - https://nomics.com',
+                validate: isRequired
+            }
+        ]);
+        const key = keyManager.setKey(input.key);
+        if(key) {
+            console.log('API Key Set'.blue);
+        }
     },
     show() {
-        console.log('Hello from show');
+        try {
+            const keyManager = new KeyManager();
+            const key = keyManager.getKey();
+            console.log('Current API Key: ', key.yellow);
+            return key;
+        } catch (err) {
+            console.error(err.message.red);    
+        }
     },
     remove() {
-        console.log('Hello from remove');
+        try {
+            const keyManager = new KeyManager();
+            keyManager.deleteKey();
+            console.log('Key Removed'.blue);
+            return;
+        } catch (err) {
+            console.error(err.message.red);    
+        }
     }
 }
 
